e03029efa834719eb62db703c8bf62b8247efbe4
Ludo-K <115109848+LudoK67@users.noreply.github.com>
2023-03-13 15:51:04 +0100

    Maj index + accueil

--- accueil.php
+++ accueil.php
@@ -1,4 +1,21 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+  header("Location: index.php");
+  exit;
+}
+
+   // Connect to the database
+   $conn = mysqli_connect('main.leskientz.ovh', 'api', 'Ludovic03', 'projet_web');
+   // Check connection
+   if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+   }
+   
+
+mysqli_close($conn);
+?>
+
 <html lang="fr">
 
 <head>
