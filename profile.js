9862761d092799355af1b5b3b87294c9c1650e3d
Bastien <bastien.zagala@viacesi.fr>
2023-03-13 16:08:50 +0100

    Commentaire et cleanning du script

--- profile.js
+++ profile.js
@@ -1,4 +1,7 @@
-let nom = "Varoquier";
+
+// Variables utilisées pour l'affichage du profil
+
+let nom = "Varoquier";                              // Variable fictives contenant les informations de l'utilisateur de la session
 let prenom = "Alanis";
 let dateNaissance = "18/06/2003";
 let pilote = "Amira";
@@ -10,46 +13,55 @@ let indeed = "jesuisunlien.indeed.fr";
 let facebook = "jesuisunlien.facebook.fr";
 let site = "jesuisunlien.site-personnel.fr";
 
-let champNom = document.getElementById("nom");
-console.log(champNom.innerHTML);
-champNom.innerHTML = "Nom : " + nom;
+// Changement du champ "nom" par la variable de l'utillisateur de la session
+
+let champNom = document.getElementById("nom");      // Récupération de l'élément HTML "nom" par son id
+champNom.innerHTML = "Nom : " + nom;                // Changement de la valeur de l'élément HTML "nom"
+
+// Changement du champ "prénom" par la variable de l'utillisateur de la session
 
 let champPrenom = document.getElementById("prenom");
-console.log(champPrenom.innerHTML);
 champPrenom.innerHTML = "Prénom : " + prenom;
 
+// Changement du champ "date de naissance" par la variable de l'utillisateur de la session
+
 let champDateNaissance = document.getElementById("date-naissance");
-console.log(champDateNaissance.innerHTML);
 champDateNaissance.innerHTML = "Date de naissance : " + dateNaissance;
 
+// Changement du champ "pilote" par la variable de l'utillisateur de la session
+
 let champPilote = document.getElementById("pilote");
-console.log(champPilote.innerHTML);
 champPilote.innerHTML = "Pilote : " + pilote;
 
 let champPromo = document.getElementById("promotion");
-console.log(champPromo.innerHTML);
 champPromo.innerHTML = "Promotion : " + promotion;
 
+// Changement du champ "promotion" par la variable de l'utillisateur de la session
+
 let champCentre = document.getElementById("centre");
-console.log(champCentre.innerHTML);
 champCentre.innerHTML = "Centre : " + centre;
 
+// Changement du champ "centre" par la variable de l'utillisateur de la session
+
 let champEntreprise = document.getElementById("entreprise");
-console.log(champEntreprise.innerHTML);
 champEntreprise.innerHTML = "Entreprise : " + entreprise;
 
+// Changement du champ "entreprise" par la variable de l'utillisateur de la session
+
 let champLinkedin = document.getElementById("linkedin");
-console.log(champLinkedin.innerHTML);
 champLinkedin.innerHTML = "<a id=\"linkedin\" href=\"" + linkedin + ".fr\"> Linkedin</a>";
 
+// Changement du lien "linkedin" par la variable de l'utillisateur de la session
+
 let champIndeed = document.getElementById("indeed");
-console.log(champIndeed.innerHTML);
 champIndeed.innerHTML = "<a id=\"indeed\" href=\"" + indeed + ".fr\"> Indeed</a>";
 
+// Changement du lien "facebook" par la variable de l'utillisateur de la session
+
 let champFacebook = document.getElementById("facebook");
-console.log(champFacebook.innerHTML);
 champFacebook.innerHTML = "<a id=\"facebook\" href=\"" + facebook + ".fr\"> Facebook</a>";
 
+// Changement du lien "site personnel" par la variable de l'utillisateur de la session
+
 let champSite = document.getElementById("site-perso");
-console.log(champSite.innerHTML);
 champSite.innerHTML = "<a id=\"site-perso\" href=\"" + site + ".fr\"> Site personnel</a>";
