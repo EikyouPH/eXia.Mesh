39f3f0cd40e6184e1d972b45c58b22b541989dde
Bastien <bastien.zagala@viacesi.fr>
2023-03-13 15:51:06 +0100

    Formatage v2

--- recap-candidatures.php
+++ recap-candidatures.php
@@ -1,15 +1,18 @@
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recap candidature</title>
-    
+
     <link rel="stylesheet" href="style.css">
 </head>
 <?php include 'header.html'; ?>
+
 <body>
-    
+
 </body>
 <?php include 'footer.php'; ?>
+
 </html>
