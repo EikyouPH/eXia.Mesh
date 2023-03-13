655dc7e105def687c76dcfd870bcae7bba4d5266
EikyouPH <bastien.zagala@viacesi.fr>
2023-03-13 11:21:05 +0100

    Process JS

--- profile.js
+++ profile.js
@@ -1,5 +1,30 @@
-let nvNom = document.getElementById("nom");
+let nom = "Varoquier";
+let prenom = "Alanis";
+let dateNaissance = "18/06/2003";
+let pilote = "Amira";
+let promotion = "A2";
 
-let nom = nvNom.innerHTML;
+let champNom = document.getElementById("nom");
+console.log(champNom.innerHTML);
 
-nvNom.innerHTML = "Varoquier";
+champNom.innerHTML = "Nom : " + nom;
+
+let champPrenom = document.getElementById("prenom");
+console.log(champPrenom.innerHTML);
+
+champPrenom.innerHTML = "Prénom : " + prenom;
+
+let champDateNaissance = document.getElementById("date-naissance");
+console.log(champDateNaissance.innerHTML);
+
+champDateNaissance.innerHTML = "Date de naissance : " + dateNaissance;
+
+let champPilote = document.getElementById("pilote");
+console.log(champPilote.innerHTML);
+
+champPilote.innerHTML = "Pilote : " + pilote;
+
+let champ = document.getElementById("pilote");
+console.log(champPilote.innerHTML);
+
+champPilote.innerHTML = "Pilote : " + pilote;
