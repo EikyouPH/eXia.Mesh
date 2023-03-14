33db9e15eefa9d1300ca58c4056b50790d882d52
Bastien <bastien.zagala@viacesi.fr>
2023-03-14 14:46:15 +0100

    Revert "modif"
    This reverts commit 56ab8636f671073cc828f4168756233137e7c8fb.

--- titre-header.js
+++ titre-header.js
@@ -1,1 +1,4 @@
-let titrePage = document.getElementByI('titre-page');
+let titrePage = document.getElementById("titre");
+
+let titreHeader = document.getElementById("titre-header");
+titreHeader.innerHTML = titrePage;
