681ceed9df0621dd154d4e82ccbee809b9b3c359
Ludo-K <115109848+LudoK67@users.noreply.github.com>
2023-03-14 15:36:08 +0100

    edit-profil.php

--- edit-profil.php
+++ edit-profil.php
@@ -1,5 +1,27 @@
 <?php include('verif-session.php'); ?>
+<?php
+session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+  header("Location: index.php");
+  exit;
+}
+// Connect to the database
+$conn = mysqli_connect('main.leskientz.ovh', 'api', 'Ludovic03', 'projet_web');
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
 
+$sql = "SELECT nom_utilisateur, prenom, password, mail, date_naissance, ID_adresse FROM utilisateur WHERE ID_utilisateur = '$_SESSION[ID_utilisateur]'";
+            $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                $nom_utilisateur = $row['nom_utilisateur'];
+                $prenom = $row['prenom'];
+                $password = $row['password'];
+                $mail = $row['mail'];
+                $date_naissance = $row['date_naissance'];
+                $perm = $row['ID_adresse'];
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
