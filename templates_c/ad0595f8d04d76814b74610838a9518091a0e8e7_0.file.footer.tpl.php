0dc3156ccf7fba147e77e87304c7fe4995687048
Bastien <bastien.zagala@viacesi.fr>
2023-03-22 14:58:58 +0100

    ni

---
+++ templates_c/ad0595f8d04d76814b74610838a9518091a0e8e7_0.file.footer.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-03-22 14:58:15
+  from 'D:\Documents\Site Projet Web\Projet-Web\tpl\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_641b0977a386e3_57927835',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ad0595f8d04d76814b74610838a9518091a0e8e7' => 
+    array (
+      0 => 'D:\\Documents\\Site Projet Web\\Projet-Web\\tpl\\footer.tpl',
+      1 => 1679393836,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_641b0977a386e3_57927835 (Smarty_Internal_Template $_smarty_tpl) {
+?><footer>
+  <table class="footer">
+    <tr>
+      <td>
+        <p>© 2023 eXia.Mesh</p>
+      </td>
+    </tr>
+    <tr>
+      <td><a href="/mentions-legales.html">Mentions légales</a>
+    </tr>
+    </td>
+    </tr>
+  </table>
+</footer><?php }
+}
