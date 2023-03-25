485d767e85a65ae45a5d750860158dc380dc7c27
alanis51 <alanis.varoquier@viacesi.fr>
2023-03-25 13:57:21 +0100

    Début bouton annuler

--- php/templates_c/5022c51114bd5d3e64b8294fa279cff9b591bf7b_0.file.edit-profil.tpl.php
+++ php/templates_c/5022c51114bd5d3e64b8294fa279cff9b591bf7b_0.file.edit-profil.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-24 16:22:16
+/* Smarty version 4.3.0, created on 2023-03-25 13:41:37
   from 'C:\Users\alani\OneDrive - Association Cesi Viacesi mail\Documents\CESI\2 - Deuxièrme année\4- Web\Projet\Site web\Projet-Web\tpl\edit-profil.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_641dc02893b9b4_92637071',
+  'unifunc' => 'content_641eec012a5554_63282060',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5022c51114bd5d3e64b8294fa279cff9b591bf7b' => 
     array (
       0 => 'C:\\Users\\alani\\OneDrive - Association Cesi Viacesi mail\\Documents\\CESI\\2 - Deuxièrme année\\4- Web\\Projet\\Site web\\Projet-Web\\tpl\\edit-profil.tpl',
-      1 => 1679671286,
+      1 => 1679748089,
       2 => 'file',
     ),
   ),
@@ -20,8 +20,8 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl) {
-?><link rel="stylesheet" href="/css/style-edit.css">
+function content_641eec012a5554_63282060 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel="stylesheet" href="/css/edit.css">
 
 <body>
     <table class="page-profil">
@@ -40,13 +40,13 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                     <input type="text" name="prenom" id="prenom" value="<?php echo $_smarty_tpl->tpl_vars['prenom']->value;?>
 " required><br>
                     <label for="mail">Adresse mail</label><br>
-                    <input type="text" name="mail" id="mail" value="<?php echo $_smarty_tpl->tpl_vars['mail']->value;?>
+                    <input type="mail" name="mail" id="mail" value="<?php echo $_smarty_tpl->tpl_vars['mail']->value;?>
 " required><br>
                     <label for="password">Mot de passe</label><br>
-                    <input type="text" name="password" id="password" value="<?php echo $_smarty_tpl->tpl_vars['password']->value;?>
+                    <input type="password" name="password" id="password" value="<?php echo $_smarty_tpl->tpl_vars['password']->value;?>
 " required><br>
                     <label for="date_naissance">Date de naissance</label><br>
-                    <input type="text" name="date_naissance" id="date_naissance" value="<?php echo $_smarty_tpl->tpl_vars['date_naissance']->value;?>
+                    <input type="date" name="date_naissance" id="date_naissance" value="<?php echo $_smarty_tpl->tpl_vars['date_naissance']->value;?>
 " required><br>
                     <label for="numero">N°</label><br>
                     <input type="text" name="numero" id="numero" value="<?php echo $_smarty_tpl->tpl_vars['numero']->value;?>
@@ -58,7 +58,7 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                     <input type="text" name="rue" id="rue" value="<?php echo $_smarty_tpl->tpl_vars['rue']->value;?>
 " required><br>
                     <label for="code_postal">Code postal</label><br>
-                    <input type="text" name="code_postal" id="code_postal" value="<?php echo $_smarty_tpl->tpl_vars['code_postal']->value;?>
+                    <input type="number" name="code_postal" id="code_postal" value="<?php echo $_smarty_tpl->tpl_vars['code_postal']->value;?>
 " required><br>
                     <label for="ville">Ville</label><br>
                     <input type="text" name="ville" id="ville" value="<?php echo $_smarty_tpl->tpl_vars['ville']->value;?>
@@ -69,6 +69,14 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                     <label for="nom_pays">Pays</label><br>
                     <input type="text" name="nom_pays" id="nom_pays" value="<?php echo $_smarty_tpl->tpl_vars['pays']->value;?>
 " required><br>
+                    <button class = "boutons" href='profil.tpl' type="submit" name="submit" class="a">Enregistrer</button>
+                    <table class="lien boutons">
+                    <tr>
+                        <td>
+                            <a target="_blank" href="./profil.php">Annuler</a>
+                        </td>
+                    </tr>
+                </table>
                 </form>
             </td>
             <td id="td-reseaux">
@@ -83,7 +91,7 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                                     <td>
                                         <label for="nom_pays">Lien Linkedin</label><br>
                                         <input type="text" name="nom_pays" id="nom_pays" value="<?php echo $_smarty_tpl->tpl_vars['lien_linkedin']->value;?>
-" required><br>
+"><br>
                                     </td>
                                 </tr>
                             </table>
@@ -99,7 +107,7 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                                     <td>
                                         <label for="nom_pays">Lien Indeed</label><br>
                                         <input type="text" name="nom_pays" id="nom_pays" value="<?php echo $_smarty_tpl->tpl_vars['lien_indeed']->value;?>
-" required><br>
+"><br>
                                     </td>
                                 </tr>
                             </table>
@@ -115,7 +123,7 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                                     <td>
                                         <label for="nom_pays">Lien Facebook</label><br>
                                         <input type="text" name="nom_pays" id="nom_pays" value="<?php echo $_smarty_tpl->tpl_vars['lien_facebook']->value;?>
-" required><br>
+"><br>
                                     </td>
                                 </tr>
                             </table>
@@ -131,19 +139,15 @@ function content_641dc02893b9b4_92637071 (Smarty_Internal_Template $_smarty_tpl)
                                     <td>
                                         <label for="nom_pays">Lien Site Personnel</label><br>
                                         <input type="text" name="nom_pays" id="nom_pays" value="<?php echo $_smarty_tpl->tpl_vars['lien_perso']->value;?>
-" required><br>
+"><br>
                                     </td>
                                 </tr>
                             </table>
                         </td>
                     </tr>
                 </table>
-                <button type="submit" name="submit" class="a">Enregistrer</button>
             </td>
         </tr>
-    </table>
-    </td>
-    </tr>
     </table>
 
     <p>
--- templates_c/133bd9bbe0c979ff5156cc474cacffe1fb33d38e_0.file.index.tpl.php
+++ templates_c/133bd9bbe0c979ff5156cc474cacffe1fb33d38e_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-22 11:17:24
+/* Smarty version 4.3.0, created on 2023-03-25 12:24:04
   from 'C:\Users\alani\OneDrive - Association Cesi Viacesi mail\Documents\CESI\2 - Deuxièrme année\4- Web\Projet\Site web\Projet-Web\tpl\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_641ad5b457e3c7_42098550',
+  'unifunc' => 'content_641ed9d47bf474_01461670',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '133bd9bbe0c979ff5156cc474cacffe1fb33d38e' => 
     array (
       0 => 'C:\\Users\\alani\\OneDrive - Association Cesi Viacesi mail\\Documents\\CESI\\2 - Deuxièrme année\\4- Web\\Projet\\Site web\\Projet-Web\\tpl\\index.tpl',
-      1 => 1679426359,
+      1 => 1679743337,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_641ad5b457e3c7_42098550 (Smarty_Internal_Template $_smarty_tpl) {
+function content_641ed9d47bf474_01461670 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="/css/style-connexion.css">
 <link rel="stylesheet" href="/css/style-footer.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
@@ -31,13 +31,11 @@ function content_641ad5b457e3c7_42098550 (Smarty_Internal_Template $_smarty_tpl)
       </div>
       <div id="conteneur">
          <form method="post" action="index.php?page=login&amp;a=connect">
-
-
-            <label for="username">Identifiant:</label>
+            <label for="username">Identifiant:</label><br>
             <input type="text" placeholder="Adresse mail" id="mail" name="mail" required><br><br>
-            <label for="password">Mot de passe:</label>
+            <label for="password">Mot de passe:</label><br>
             <input type="password" placeholder="Mot de passe" id="password" name="password" required><br><br>
-            <input type="submit" name="submit" value="Submit">
+            <input type="submit" name="submit" value="Connexion" class="a">
          </form>
       </div>
 </body><?php }
