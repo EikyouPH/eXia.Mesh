62212e076cd3a77454dcf6e6472134f88f822e5a
alanis51 <alanis.varoquier@viacesi.fr>
2023-03-27 20:22:40 +0200

    maj edit profil

--- php/templates_c/9e00063adfab766d75945a9e5e74d6c753bb997d_0.file.edit-profil-etudiant.tpl.php
+++ php/templates_c/9e00063adfab766d75945a9e5e74d6c753bb997d_0.file.edit-profil-etudiant.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-27 16:49:44
+/* Smarty version 4.3.0, created on 2023-03-27 20:04:24
   from 'C:\Users\alani\OneDrive - Association Cesi Viacesi mail\Documents\CESI\2 - Deuxièrme année\4- Web\Projet\Site web\Projet-Web\tpl\edit-profil-etudiant.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_6421ad08d18668_26078276',
+  'unifunc' => 'content_6421daa8c1c0f5_26157143',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '9e00063adfab766d75945a9e5e74d6c753bb997d' => 
     array (
       0 => 'C:\\Users\\alani\\OneDrive - Association Cesi Viacesi mail\\Documents\\CESI\\2 - Deuxièrme année\\4- Web\\Projet\\Site web\\Projet-Web\\tpl\\edit-profil-etudiant.tpl',
-      1 => 1679922269,
+      1 => 1679940262,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6421ad08d18668_26078276 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6421daa8c1c0f5_26157143 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="/css/edit.css">
 <link rel="stylesheet" href="/css/listes-deroulantes.css">
 <body>
@@ -33,39 +33,56 @@ function content_6421ad08d18668_26078276 (Smarty_Internal_Template $_smarty_tpl)
                 <form method="post" autocomplete="off">
                     <label for="nom_utilisateur">Nom</label><br>
                     <input type="text" name="nom_utilisateur" id="nom_utilisateur" value="<?php echo $_smarty_tpl->tpl_vars['nom_utilisateur']->value;?>
-"
-                        required><br>
+" required><br>
 
                     <label for="prenom">Prénom</label><br>
                     <input type="text" name="prenom" id="prenom" value="<?php echo $_smarty_tpl->tpl_vars['prenom']->value;?>
 " required><br>
+
                     <label for="mail">Adresse mail</label><br>
                     <input type="mail" name="mail" id="mail" value="<?php echo $_smarty_tpl->tpl_vars['mail']->value;?>
 " required><br>
+
                     <label for="password">Mot de passe</label><br>
                     <input type="password" name="password" id="password" value="<?php echo $_smarty_tpl->tpl_vars['password']->value;?>
 " required><br>
+                    
+                    <label for="nom_promo">Promotion</label><br>
+                    <input type="text" name="nom_promo" id="nom_promo" value="<?php echo $_smarty_tpl->tpl_vars['nom_promo']->value;?>
+" required><br>
+
+                    <label for="nom_centre">Nom du centre</label><br>
+                    <input type="text" name="nom_centre" id="nom_centre" value="<?php echo $_smarty_tpl->tpl_vars['nom_centre']->value;?>
+" required><br>
+
                     <label for="date_naissance">Date de naissance</label><br>
                     <input type="date" name="date_naissance" id="date_naissance" value="<?php echo $_smarty_tpl->tpl_vars['date_naissance']->value;?>
 " required><br>
+
                     <label for="numero">N°</label><br>
                     <input type="text" name="numero" id="numero" value="<?php echo $_smarty_tpl->tpl_vars['numero']->value;?>
 " required><br>
+
                     <label for="complement">Complément</label><br>
                     <input type="text" name="complement" id="complement" value="<?php echo $_smarty_tpl->tpl_vars['complement']->value;?>
 "><br>
+
                     <label for="rue">Rue</label><br>
                     <input type="text" name="rue" id="rue" value="<?php echo $_smarty_tpl->tpl_vars['rue']->value;?>
 " required><br>
+
                     <label for="code_postal">Code postal</label><br>
                     <input type="number" name="code_postal" id="code_postal" value="<?php echo $_smarty_tpl->tpl_vars['code_postal']->value;?>
 " required><br>
+
                     <label for="ville">Ville</label><br>
                     <input type="text" name="ville" id="ville" value="<?php echo $_smarty_tpl->tpl_vars['ville']->value;?>
 " required><br>
+
                     <label for="region">Région</label><br>
                     <input type="text" name="region" id="region" value="<?php echo $_smarty_tpl->tpl_vars['region']->value;?>
-" required><br>'
+" required><br>
+
                     <label for="pays">Pays</label><br>
                     <input type="text" name="nom_pays" id="pays" value="<?php echo $_smarty_tpl->tpl_vars['pays']->value;?>
 " placeholder="Votre pays..."><br>
---
+++ php/templates_c/f0a593110562c9ad5d0c65787dda7c18789d9bc9_0.file.profil-recruteur.tpl.php
@@ -0,0 +1,122 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-03-27 20:07:23
+  from 'C:\Users\alani\OneDrive - Association Cesi Viacesi mail\Documents\CESI\2 - Deuxièrme année\4- Web\Projet\Site web\Projet-Web\tpl\profil-recruteur.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_6421db5bceec59_20193187',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f0a593110562c9ad5d0c65787dda7c18789d9bc9' => 
+    array (
+      0 => 'C:\\Users\\alani\\OneDrive - Association Cesi Viacesi mail\\Documents\\CESI\\2 - Deuxièrme année\\4- Web\\Projet\\Site web\\Projet-Web\\tpl\\profil-recruteur.tpl',
+      1 => 1679922269,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6421db5bceec59_20193187 (Smarty_Internal_Template $_smarty_tpl) {
+?><link rel="stylesheet" href="/css/profil.css">
+
+<body>
+    <table class="page-profil">
+        <tr>
+            <td class="personne">
+                <img src="/img/personne.png" alt="Image du compte">
+            </td>
+            <td>
+                Vous êtes Recruteur<br><br>
+                Nom : <?php echo $_smarty_tpl->tpl_vars['nom_utilisateur']->value;?>
+<br>
+                Prenom : <?php echo $_smarty_tpl->tpl_vars['prenom']->value;?>
+<br>
+                Représente : <?php echo $_smarty_tpl->tpl_vars['nom_entreprise']->value;?>
+<br>
+                Adresse mail : <?php echo $_smarty_tpl->tpl_vars['mail']->value;?>
+<br>
+                Date de naissance : <?php echo $_smarty_tpl->tpl_vars['date_naissance']->value;?>
+<br>
+                Adresse : <?php echo $_smarty_tpl->tpl_vars['numero']->value;?>
+ <?php echo $_smarty_tpl->tpl_vars['complement']->value;?>
+ <?php echo $_smarty_tpl->tpl_vars['rue']->value;?>
+,<br>
+                <?php echo $_smarty_tpl->tpl_vars['code_postal']->value;?>
+, <?php echo $_smarty_tpl->tpl_vars['ville']->value;?>
+, <?php echo $_smarty_tpl->tpl_vars['region']->value;?>
+, <?php echo $_smarty_tpl->tpl_vars['pays']->value;?>
+<br>
+            </td>
+            <td id="td-reseaux">
+                <table class="reseaux">
+                    <a href="edit_profil.php"><img src="/img/stylo.png" alt="editer" id="crayon" width="40"></a>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="icone">
+                                        <img src="/img/linkedin.png" alt="">
+                                    </td>
+                                    <td>
+                                        <a id="linkedin" href="<?php echo $_smarty_tpl->tpl_vars['lien_linkedin']->value;?>
+"> LinkedIn</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="icone">
+                                        <img src="/img/indeed.png" alt="">
+                                    </td>
+                                    <td>
+                                        <a id="indeed" href="<?php echo $_smarty_tpl->tpl_vars['lien_indeed']->value;?>
+"> Indeed</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="icone">
+                                        <img src="/img/facebook.png" alt="">
+                                    </td>
+                                    <td>
+                                        <a id="facebook" href="<?php echo $_smarty_tpl->tpl_vars['lien_facebook']->value;?>
+"> Facebook</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td class="icone">
+                                        <img src="/img/site.png" alt="">
+                                    </td>
+                                    <td>
+                                        <a id="site-perso" href="<?php echo $_smarty_tpl->tpl_vars['lien_perso']->value;?>
+"> Site
+                                            personnel</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <p>
+        Expériences passées
+    </p>
+</body><?php }
+}
