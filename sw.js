f72d3c23afe1e32558230c2e858f7d227b6fc20b
alanis51 <alanis.varoquier@viacesi.fr>
2023-03-28 15:39:46 +0200

    sync

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
 // installe le service worker
 
 self.addEventListener('install', evt =>  {
-    console.log('Service worker installé.')
+    console.log('Service worker installé !')
 })
 
 // active le service worker
+self.addEventListener('activate', evt =>  {
+    console.log('Service worker activé !')
+})
