0b97dc6bc5a686afcb6dd1ff8a8b3e445c9c6a4e
Ludo-K <ludovic.kientz@gmail.com>
2023-03-28 21:12:04 +0200

    edit-admin

--- php/admin-edit.php
+++ php/admin-edit.php
@@ -29,7 +29,7 @@ if ($role == 'etudiant') {
 }
 
 if ($role == 'recruteur') {
-    $sql = "SELECT nom_entreprise FROM entreprise WHERE ID_entreprise = '$_SESSION[ID_entreprise]'";
+    $sql = "SELECT nom_entreprise FROM entreprise JOIN recruteur ON entreprise.ID_entreprise = recruteur.ID_entreprise WHERE recruteur.ID_recruteur = $ID_role";
     $result = $conn->query($sql);
     $row = $result->fetch(PDO::FETCH_ASSOC);
     $nom_entreprise = $row['nom_entreprise'];
@@ -89,10 +89,6 @@ $smarty->assign('region', $region);
 $smarty->assign('pays', $pays);
 $smarty->assign('titre_onglet', 'Edition du profil');
 $smarty->assign('titre_page', 'Edition du profil');
-
-$role = $_SESSION['role'];
-
-
 
 if (isset($_POST['submit'])) {
 
