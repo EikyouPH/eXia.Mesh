5976e8f79b0446007217724253dc26e5d5ca40d7
alanis51 <alanis.varoquier@viacesi.fr>
2023-03-29 10:28:12 +0200

    notification responsive

--- php/templates_c/688f3afc0962918cb06fd35fceaf4fe3cd55da3b_0.file.notifications.tpl.php
+++ php/templates_c/688f3afc0962918cb06fd35fceaf4fe3cd55da3b_0.file.notifications.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-27 09:09:55
+/* Smarty version 4.3.0, created on 2023-03-29 10:27:56
   from 'C:\Users\alani\OneDrive - Association Cesi Viacesi mail\Documents\CESI\2 - Deuxièrme année\4- Web\Projet\Site web\Projet-Web\tpl\notifications.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_6421414321c916_48372276',
+  'unifunc' => 'content_6423f68cc31133_17649905',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '688f3afc0962918cb06fd35fceaf4fe3cd55da3b' => 
     array (
       0 => 'C:\\Users\\alani\\OneDrive - Association Cesi Viacesi mail\\Documents\\CESI\\2 - Deuxièrme année\\4- Web\\Projet\\Site web\\Projet-Web\\tpl\\notifications.tpl',
-      1 => 1679765196,
+      1 => 1680078456,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6421414321c916_48372276 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6423f68cc31133_17649905 (Smarty_Internal_Template $_smarty_tpl) {
 ?><link rel="stylesheet" href="/css/notifications.css">
 
 <body>
@@ -36,19 +36,16 @@ function content_6421414321c916_48372276 (Smarty_Internal_Template $_smarty_tpl)
                         </div>
                         <div class="notification-content">
                             <a href="notifications.php" target="_blank">
-                                <div class="notification-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Sed
-                                    euismod,
-                                    nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit
-                                    amet
-                                    nisl.
-                                    Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam
-                                    nisl
-                                    nisl
-                                    sit
-                                    amet nisl.</div>
+                                <div class="notification-text">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit
+                                        amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
+                                        nisl. Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget
+                                        aliquam nisl nisl sit amet nisl.
+                                    </p>
+                                </div>
                                 <div class="notification-link"><a href="#"><img src="/img/fleche-droite.svg"
-                                            alt="Fleche vers la droite"></a></div>
+                                            alt="Fleche vers la droite"></a>
+                                </div>
                         </div>
                     </div>
                 </a>
@@ -59,13 +56,13 @@ function content_6421414321c916_48372276 (Smarty_Internal_Template $_smarty_tpl)
                         <div class="notification-date">Date</div>
                     </div>
                     <div class="notification-content">
-                        <div class="notification-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                            euismod,
-                            nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
-                            nisl.
-                            Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl
-                            sit
-                            amet nisl.</div>
+                        <div class="notification-text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit
+                                amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
+                                nisl. Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget
+                                aliquam nisl nisl sit amet nisl.
+                            </p>
+                        </div>
                         <div class="notification-link"><a href="#"><a href="#"><img src="/img/fleche-droite.svg"
                                         alt="Fleche vers la droite"></a></a></div>
                     </div>
@@ -77,13 +74,13 @@ function content_6421414321c916_48372276 (Smarty_Internal_Template $_smarty_tpl)
                         <div class="notification-date">Date</div>
                     </div>
                     <div class="notification-content">
-                        <div class="notification-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                            euismod,
-                            nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
-                            nisl.
-                            Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl
-                            sit
-                            amet nisl.</div>
+                        <div class="notification-text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit
+                                amet ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet
+                                nisl. Sed euismod, nisl sit amet ultricies lacinia, nisl nisl aliquam nisl, eget
+                                aliquam nisl nisl sit amet nisl.
+                            </p>
+                        </div>
                         <div class="notification-link"><a href="#"><img src="/img/fleche-droite.svg"
                                     alt="Fleche vers la droite"></a><a href="#">
                         </div>
